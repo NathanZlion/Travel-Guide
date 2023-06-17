c70eaa8ffcd2ecd02f1c552d8ba890e1586ce099
Nathnael Dereje <nathandere1357@gmail.com>
2023-06-18 01:52:40 +0300

    modified to give the front end a populated json data instead of tje reference id

--- backend/routes/cartRouter.js
+++ backend/routes/cartRouter.js
@@ -8,21 +8,16 @@ import {
     removeHotelFromCart,
     removeRestaurantFromCart,
     removeDestinationFromCart,
-    getHotels,
-    getDestinations,
-    getRestaurants,
+    getCart,
 } from '../controllers/cartController.js';
 
 const cartRouter = express.Router();
 
+cartRouter.get('/', getCart);
 cartRouter.post('/addHotel', addHotelToCart);
 cartRouter.post('/addRestaurant', addRestaurantToCart);
 cartRouter.post('/addDestination', addDestinationToCart);
 cartRouter.post('/removeHotel', removeHotelFromCart);
 cartRouter.post('/removeRestaurant', removeRestaurantFromCart);
 cartRouter.post('/removeDestination', removeDestinationFromCart);
-cartRouter.get('/Hotels', getHotels);
-cartRouter.get('/Destinations', getDestinations);
-cartRouter.get('/Restaurants', getRestaurants);
-
 export default cartRouter;
